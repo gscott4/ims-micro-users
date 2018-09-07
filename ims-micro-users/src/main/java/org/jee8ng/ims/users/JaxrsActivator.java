e9ed24fb35de6dd5c7de89d6e57a1140af232b48
grays <grays@LAPTOP-7OJD8FRQ.attlocal.net>
2018-09-06 20:30:47 -0400

    First commit

---
+++ ims-micro-users/src/main/java/org/jee8ng/ims/users/JaxrsActivator.java
@@ -0,0 +1,9 @@
+package org.jee8ng.ims.users;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JaxrsActivator extends Application {
+
+}
